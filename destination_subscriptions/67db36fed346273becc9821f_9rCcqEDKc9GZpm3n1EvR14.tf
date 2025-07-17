d692d959aaf8190d1167c1759df8282a70f1c117
Segment Git Sync <friends@segment.com>
2025-07-17 15:50:36 +0000

    Destination Subscription '9rCcqEDKc9GZpm3n1EvR14' modified by ehyde@twilio.com

--- destination_subscriptions/67db36fed346273becc9821f_9rCcqEDKc9GZpm3n1EvR14.tf
+++ destination_subscriptions/67db36fed346273becc9821f_9rCcqEDKc9GZpm3n1EvR14.tf
@@ -11,10 +11,13 @@ resource "segment_destination_subscription" "id-67db36fed346273becc9821f_9rCcqED
   name                 = "Post Message"
   reverse_etl_schedule = null
   settings = jsonencode({
+    channel  = "#general"
     icon_url = "https://logo.clearbit.com/segment.com"
-    text     = "adsfasdfad"
-    url      = "asdfasdfasdfa"
+    text = {
+      "@template" = "Source Created with name: *{{properties.details.target}} * by {{properties.details.subject_email}}"
+    }
+    url      = "https://hooks.slack.com/services/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"
     username = "Segment"
   })
-  trigger = "event = \"Audit\""
+  trigger = "event = \"Audit\" and properties.type = \"Source Created\""
 }
