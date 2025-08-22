179b17e64046fe8fb7edfc32eddeac345fee767c
Segment Git Sync <friends@segment.com>
2025-08-22 17:45:38 +0000

    Reverse ETL Model '5yqPq4KNoDzpgkfQhWSKFa' created by ehyde@twilio.com

---
+++ reverse_etl_models/5yqPq4KNoDzpgkfQhWSKFa.tf
@@ -0,0 +1,13 @@
+import {
+  to = segment_reverse_etl_model.id-5yqPq4KNoDzpgkfQhWSKFa
+  id = "5yqPq4KNoDzpgkfQhWSKFa"
+}
+
+resource "segment_reverse_etl_model" "id-5yqPq4KNoDzpgkfQhWSKFa" {
+  description             = "Programatically generated rETL model that is associated with an Engage rETL audience"
+  enabled                 = true
+  name                    = "Linked Audience rETL Model"
+  query                   = "SELECT 1"
+  query_identifier_column = "matchId"
+  source_id               = "wgTDxNQhwPoryz8Uch5TEB"
+}
