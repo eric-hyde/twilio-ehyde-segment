278af30cc5f0da05bc69dc9bb80580ee6bf46c6e
Segment Git Sync <friends@segment.com>
2025-11-19 19:09:47 +0000

    User 'nfZ5Dr61HhmUD1wdVmGroP' added by ptuhtan@twilio.com

---
+++ users/nfZ5Dr61HhmUD1wdVmGroP.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-nfZ5Dr61HhmUD1wdVmGroP
+  id = "nfZ5Dr61HhmUD1wdVmGroP"
+}
+
+resource "segment_user" "id-nfZ5Dr61HhmUD1wdVmGroP" {
+  email = "ptuhtan@twilio.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "fBZpJYpdxcKa4qgtnbW5b6"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DFMmZbyzdEWmE6k8JVgKAiE"
+    },
+  ]
+}
