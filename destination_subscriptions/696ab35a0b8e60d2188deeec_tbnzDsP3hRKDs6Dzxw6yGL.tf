1cf9550ce81957eac6af1033de159e5a6f46af22
Segment Git Sync <friends@segment.com>
2026-01-16 21:53:51 +0000

    Destination Subscription 'tbnzDsP3hRKDs6Dzxw6yGL' created by ahoyle@twilio.com

---
+++ destination_subscriptions/696ab35a0b8e60d2188deeec_tbnzDsP3hRKDs6Dzxw6yGL.tf
@@ -0,0 +1,29 @@
+import {
+  to = segment_destination_subscription.id-696ab35a0b8e60d2188deeec_tbnzDsP3hRKDs6Dzxw6yGL
+  id = "696ab35a0b8e60d2188deeec:tbnzDsP3hRKDs6Dzxw6yGL"
+}
+
+resource "segment_destination_subscription" "id-696ab35a0b8e60d2188deeec_tbnzDsP3hRKDs6Dzxw6yGL" {
+  action_id            = "2Xg7PHV6kQDWdS8j2da66P"
+  destination_id       = "696ab35a0b8e60d2188deeec"
+  enabled              = true
+  model_id             = null
+  name                 = "Audience Entered (SFTP)"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    audience_key = {
+      "@path" = "$.userId"
+    }
+    batch_size      = 50000
+    delimiter       = ","
+    enable_batching = true
+    filename = {
+      "@template" = "{{properties.audience_key}}_PII.csv"
+    }
+    sftp_folder_path = {
+      "@template" = "/uploads/"
+    }
+    sftp_password = "sdfsdf"
+  })
+  trigger = "event = \"Audience Entered\""
+}
