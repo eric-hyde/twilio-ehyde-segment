f47d0c3add3a7e425a6f0030c934196f59ee4717
Segment Git Sync <friends@segment.com>
2026-01-16 20:19:26 +0000

    Destination Subscription 'gmzPFPBwZrbAT35FScF9E5' created by ahoyle@twilio.com

---
+++ destination_subscriptions/696a9c9c2880f687def5d17e_gmzPFPBwZrbAT35FScF9E5.tf
@@ -0,0 +1,29 @@
+import {
+  to = segment_destination_subscription.id-696a9c9c2880f687def5d17e_gmzPFPBwZrbAT35FScF9E5
+  id = "696a9c9c2880f687def5d17e:gmzPFPBwZrbAT35FScF9E5"
+}
+
+resource "segment_destination_subscription" "id-696a9c9c2880f687def5d17e_gmzPFPBwZrbAT35FScF9E5" {
+  action_id            = "2Xg7PHV6kQDWdS8j2da66P"
+  destination_id       = "696a9c9c2880f687def5d17e"
+  enabled              = true
+  model_id             = null
+  name                 = "Audience Entered (SFTP) Andrew Hoyle test"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    audience_key = {
+      "@path" = "$.userId"
+    }
+    batch_size      = 50000
+    delimiter       = ","
+    enable_batching = true
+    filename = {
+      "@template" = "{{properties.audience_key}}_PII.csv"
+    }
+    sftp_folder_path = {
+      "@template" = "/uploads/"
+    }
+    sftp_password = "sdfsdfs"
+  })
+  trigger = "event = \"Audience Entered\""
+}
