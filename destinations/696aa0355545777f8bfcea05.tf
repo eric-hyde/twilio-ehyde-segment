83d6041fa99737165e43216cb85971b6dc604215
Segment Git Sync <friends@segment.com>
2026-01-16 21:56:32 +0000

    Destination '696aa0355545777f8bfcea05' modified by ahoyle@twilio.com

--- destinations/696aa0355545777f8bfcea05.tf
+++ destinations/696aa0355545777f8bfcea05.tf
@@ -12,10 +12,16 @@ resource "segment_destination" "id-696aa0355545777f8bfcea05" {
     region_endpoints  = ["US"]
     supported_regions = ["us-west-2", "eu-west-1"]
   }
-  name = "Andrew Hoyle test TTD"
+  name = "Andrew Hoyle TTD Personas"
   settings = jsonencode({
-    advertiser_id = ""
-    auth_token    = ""
+    advertiser_id = "sdf"
+    auth_token    = "••••••••••"
+    dynamicAuthSettings = {
+      configId = "696aa0355545777f8bfcea05"
+      oauth = {
+        type = "noAuth"
+      }
+    }
   })
   source_id = "kqy6SqXM24c3mPc3XFgdgg"
 }
